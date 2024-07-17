bd39b94d68abb33ac662cc04fe99192ca909fabb
askaryxiii <ahm.alaskary@gmail.com>
2024-07-17 17:21:44 +0300

    17/7/2024
    Finished the logic

---
+++ js/javascript.js
@@ -0,0 +1,21 @@
+function getNumber(index, type) {
+  var intialNumbers = document.querySelectorAll(".numbers");
+  var intialOperators = document.querySelectorAll(".operators");
+  if ((type == true)) {
+    var number = +intialNumbers[index].innerHTML;
+    displayInResult(number);
+  } else if ((type == false)) {
+    var operator = intialOperators[index].innerHTML;
+    displayInResult(operator);
+  }
+}
+
+function displayInResult(dispalyedObject) {
+  var displayedResult = document.querySelector(".result");
+  displayedResult.innerHTML += dispalyedObject;
+}
+
+function deleteAll(deleteAllResult) {
+  var displayedResult = document.querySelector(".result");
+  displayedResult.innerHTML = "";
+}
