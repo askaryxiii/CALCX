f5d7d259b5293b8034e9939bbffbfa7b5b6ff460
Ahmed Khaled Al-Askary <ahm.alaskary@gmail.com>
2024-07-20 18:11:33 +0300

    20/7/2024
    making the code dynamic by forEach and addEventListener

--- js/javascript.js
+++ js/javascript.js
@@ -1,48 +1,16 @@
-/*
-1-design buttons and a input field to show result at
-2-logic part
-  2.1 - get numbers in an array and operators in seperate array
-  2.2 - when you press the button sent the value of the button to logic
-    2.2.0 - filter the value if it's a number or an operator   
-  2.3 - procces the value and make it a number
-  2.4 - show it in the input field
-  2.5 - make delete button return the input field empty
-3-put some style to your HTML
-*/
-
-
-
-
 
 // getting array variables with buttons value
-var intialNumbers = document.querySelectorAll(".numbers");
-var intialOperators = document.querySelectorAll(".operators");
+var intialNumbers = document.querySelectorAll(".numerical");
+var intialOperators = document.querySelectorAll(".operation");
 
-// select result input field
+// // select result input field
 var displayedResult = document.querySelector(".result");
 
 
+// printing the button number in result section
+intialNumbers.forEach((numericals) => {
+  numericals.addEventListener('click', (number) => {
+    displayedResult.value += number.target.innerText
+  })
+})
 
-// filter the the pressed button's value
-function getInput(index, type) {
-  if ((type == true)) {
-    // numbers returns an index number and true indicating it's a numbers
-    var number = +intialNumbers[index].innerHTML;
-    displayInResult(number);
-  } else if ((type == false)) {
-    // operators returns an index number and false indicating it's an operators
-    var operator = intialOperators[index].innerHTML;
-    displayInResult(operator);
-  }
-}
-
-
-// display the pressed button in input field
-function displayInResult(dispalyedObject) {
-  displayedResult.value = displayedResult.value + dispalyedObject;
-}
-
-// return input field empty
-function deleteAll() {
-  displayedResult.value = "";
-}
