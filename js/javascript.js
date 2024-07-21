06c3e97513082cb49858c54bc6f37ee8b2191f1b
Ahmed Khaled Al-Askary <ahm.alaskary@gmail.com>
2024-07-21 23:25:14 +0300

    21/7/2024
    improving design

--- js/javascript.js
+++ js/javascript.js
@@ -2,6 +2,7 @@
 // getting array variables with buttons value
 var intialNumbers = document.querySelectorAll(".numerical");
 var intialOperators = document.querySelectorAll(".operation");
+var deleteAll = document.querySelector(".deleteAll")
 
 // // select result input field
 var displayedResult = document.querySelector(".result");
@@ -12,5 +13,11 @@ intialNumbers.forEach((numericals) => {
   numericals.addEventListener('click', (number) => {
     displayedResult.value += number.target.innerText
   })
+})
+
+
+// return the result section into intial form
+deleteAll.addEventListener('click', () => {
+  displayedResult.value = ""
 })
 
