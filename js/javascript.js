4407ada06285cbabad603a20067032a519cfd1e6
askaryxiii <ahm.alaskary@gmail.com>
2024-08-01 14:29:32 +0300

    1/8/2024
    Calculate and print the true result

--- js/javascript.js
+++ js/javascript.js
@@ -1,23 +1,24 @@
-
 // getting array variables with buttons value
-var intialNumbers = document.querySelectorAll(".numerical");
-var intialOperators = document.querySelectorAll(".operation");
-var deleteAll = document.querySelector(".deleteAll")
+let intialNumbers = document.querySelectorAll(".numerical");
+let deleteAll = document.querySelector(".deleteAll");
+let calculation = document.querySelector(".calculation");
 
 // // select result input field
 var displayedResult = document.querySelector(".result");
 
-
 // printing the button number in result section
 intialNumbers.forEach((numericals) => {
-  numericals.addEventListener('click', (number) => {
-    displayedResult.value += number.target.innerText
-  })
-})
+  numericals.addEventListener("click", (number) => {
+    displayedResult.value += number.target.innerText;
+  });
+});
 
+//printing the calculation result
+calculation.addEventListener("click", () => {
+  displayedResult.value = eval(displayedResult.value);
+});
 
 // return the result section into intial form
-deleteAll.addEventListener('click', () => {
-  displayedResult.value = ""
-})
-
+deleteAll.addEventListener("click", () => {
+  displayedResult.value = "";
+});
